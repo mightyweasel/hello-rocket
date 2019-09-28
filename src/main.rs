7e17a65d17abfb894a65a1413e772a34d228448e
Sinan Baltacioglu <siphon@gmail.com>
2019-09-27 20:17:51 -0400

    Hello Route

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[get("/hello/<name>/<age>")]
+fn hello(name: String, age: u8) -> String {
+    format!(
+        "Hello, human named {}. You have cycled {} times.",
+        name, age
+    )
+}
+
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite().mount("/", routes![index, hello]).launch();
 }
